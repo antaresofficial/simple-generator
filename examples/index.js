45fa27b934a9609bc1575fbedef93b8c949dc558
Maria Berestovaya <57846761+antaresofficial@users.noreply.github.com>
2023-02-24 16:15:49 +0500

    Minor changes.

--- examples/index.js
+++ examples/index.js
@@ -1,10 +1,5 @@
 import generateImage from "../index.js";
 import { collection } from "./collection.js";
 
-const getImage = async () => {
-  const file = await generateImage("../assets/", collection, "./public/");
-  console.log(file);
-}
-
-getImage()
+await generateImage("../assets/", collection, "./public/");
 
--- index.js
+++ index.js
@@ -3,34 +3,32 @@ import { writeFileSync, readdirSync } from "fs";
 import sizeOf from "image-size";
 import { v4 as uuidv4 } from 'uuid';
 
-const createKeys = (root) => readdirSync(root).map((folder) => {
+const CANVAS_SIZE = [440, 440];
+
+const generateRandomFolderMap = (root) => readdirSync(root).map((folder) => {
   const files = readdirSync(`${root}${folder}`);
   const index = Math.floor(Math.random() * files.length);
   return { [folder]: `${root}${folder}/${files[index]}` };
 });
 
 const generateImage = async (rootFolder, collection, path) => {
-  const canvas = createCanvas(440, 440);
+  const canvas = createCanvas(...CANVAS_SIZE);
   const ctx = canvas.getContext("2d", { alpha: false });
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  const result = [];
-  createKeys(rootFolder).forEach(async (el) => {
-    collection.forEach((item) => item.name === Object.keys(el)[0] && result.push({ ...item, path: Object.values(el)[0] }));
-  });
-  result.sort((a, b) => a.layer - b.layer);
+  const metadate = [];
+  generateRandomFolderMap(rootFolder).forEach(async (el) => collection.forEach((item) => item.name === Object.keys(el)[0] && metadate.push({ ...item, path: Object.values(el)[0] })));
+  metadate.sort((a, b) => a.layer - b.layer);
 
-  await result.forEach(async ({ path, top, left }) => {
+  await new Promise((resolve) => metadate.forEach(async ({ path, top, left = 220 }) => {
     const { width } = sizeOf(path);
     const entity = await loadImage(path);
-    const leftPadding = typeof left === "number" ? left : 220 - width / 2;
+    const leftPadding = left - width / 2;
     ctx.drawImage(entity, leftPadding, top);
-  });
+    resolve();
+  }));
 
-  const file = `image-${uuidv4()}.png`;
-
-  writeFileSync(`${path}${file}`, canvas.toBuffer("image/png"));
-  return file;
+  writeFileSync(`${path}image-${uuidv4()}.png}`, canvas.toBuffer("image/png"));
 };
 
 export default generateImage;
